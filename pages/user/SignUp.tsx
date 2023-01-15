32a2eb70f60f99feb4877eef9ee9ffac9c2c6c0c
MubeenYasin <mubeen.yasin@gmail.com>
2023-01-15 20:13:11 +0300

    Step 2

---
+++ pages/user/SignUp.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+    return (
+        <div className='w-1/2 h-96 bg-slate-500'>
+            SignUp
+        </div>
+    )
+}
+
+export default SignUp
